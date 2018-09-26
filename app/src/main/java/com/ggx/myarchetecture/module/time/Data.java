37682f7e4ee8ac46fc4f27d59119f387b75ca029
gaoguanxiong <875305085@QQ.com>
2018-09-26 17:43:17 +0800

    realm temp

---
+++ app/src/main/java/com/ggx/myarchetecture/module/time/Data.java
@@ -0,0 +1,8 @@
+package com.ggx.myarchetecture.module.time;
+
+/**
+ * Created by ggx on 2018/8/17.
+ */
+
+public class Data {
+}
---
+++ domain/src/main/java/com/ggx/myapplication/executor/module/database/SaveTimeModule.java
@@ -0,0 +1,8 @@
+package com.ggx.myapplication.executor.module.database;
+
+/**
+ * Created by ggx on 2018/8/17.
+ */
+
+public class SaveTimeModule {
+}
